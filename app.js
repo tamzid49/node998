4cd45d8b48299de18ddc831f778e6d075e940f3c
tamzid49 <46148776+tamzid49@users.noreply.github.com>
2020-09-05 13:39:08 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello Whdjdbdjdnrld</h1>');
+  if(req.url == "/.well-known/pki-validation/DB54CC8EC1C2B15228458243FF2A93DC.txt"){
+     res.end("E16C386B319DD68031EA646D0897A64A7D9A303B014F05791DB0B5527D0DD363\ncomodoca.com\nbcb6daa9e6a9650");
+  }
+  res.end(req.url);
 });
 
 server.listen(port,() => {
