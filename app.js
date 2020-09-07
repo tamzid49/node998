a9b708fee11ae3b2e0a7b54fa18278a1caa406dc
tamzid49 <46148776+tamzid49@users.noreply.github.com>
2020-09-07 14:38:31 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  if(req.url == "/.well-known/pki-validation/DB54CC8EC1C2B15228458243FF2A93DC.txt"){
-     res.end("E16C386B319DD68031EA646D0897A64A7D9A303B014F05791DB0B5527D0DD363\ncomodoca.com\nbcb6daa9e6a9650");
-  }
-  res.end(req.url);
-});
+http.createServer(function (req, res) {
+	var q = url.parse(req.url, true);
+	var t_url = q.pathname;
+  	
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+	if(fs.existsSync((t_url + "/index.js").substring(1))){		
+		var filename = path.resolve("." + t_url + "/index.js");
+		delete require.cache[filename];
+		var app_file = require("." + t_url + "/index.js");
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(app_file.main(req.url));
+	}else{
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.write("Not Found");
+		console.log("its null!");
+	}
+
+	res.end();
+}).listen(8080);
