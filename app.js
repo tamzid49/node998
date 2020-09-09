cd265ab44e38eff5d595f2aadb7479b3d15b955c
tamzid49 <46148776+tamzid49@users.noreply.github.com>
2020-09-09 12:00:30 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var path = require('path');
 http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
 	var t_url = q.pathname;
-  	
+  	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("sadasdasdasdas");
 
-	if(fs.existsSync((t_url + "/index.js").substring(1))){		
+	/**if(fs.existsSync((t_url + "/index.js").substring(1))){		
 		var filename = path.resolve("." + t_url + "/index.js");
 		delete require.cache[filename];
 		var app_file = require("." + t_url + "/index.js");
@@ -19,6 +20,7 @@ http.createServer(function (req, res) {
 		res.write("Not Found");
 		console.log("its null!");
 	}
+	**/
 
 	res.end();
 }).listen(8080);
